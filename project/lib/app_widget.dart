81b2012b9f445bf6c113b02d7c230cf1c6c2ba98
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-14 11:49:40 -0300

    AppWidget refatorado

--- project/lib/app_widget.dart
+++ project/lib/app_widget.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:project/pages/account/login_page.dart';
-import 'package:project/pages/addPet/name_add_pet.dart';
-import 'package:project/pages/addPet/photo_add_pet.dart';
-import 'package:project/pages/addPet/ration_add_pet.dart';
-import 'package:project/pages/addPet/size_add_pet.dart';
-import 'package:project/pages/addPet/type_add_pet.dart';
 import 'package:project/pages/configurations/user_page.dart';
 import 'package:project/pages/splash_page.dart';
 
@@ -19,11 +13,6 @@ class AppWidget extends StatelessWidget {
       routes: {
         '/userPage': (context) => UserPage(),
         '/login': (context) => LoginPage(),
-        '/type': (context) => TypeAddPet(),
-        '/size': (context) => SizeAddPet(),
-        '/ration': (context) => RationAddPet(),
-        '/photo': (context) => PhotoAddPet(),
-        '/name': (context) => NameAddPet()
       },
     );
   }
